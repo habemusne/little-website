e472e6f33ae572f1fba235ded0a6ae88c313d383
Mark <itisnanful@gmail.com>
2016-03-14 20:32:32 -0700

    fix_bug

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Markchen::Application.initialize!
-config.action_dispatch.x_sendfile_header = "X-Sendfile"
