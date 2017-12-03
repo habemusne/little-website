59c854f2b3c43517d575a4b915be1ccc9b506ec0
habemusne <itisnanful@gmail.com>
2017-12-03 00:38:05 -0800

    nuxt

--- db/migrate/20160315020351_create_visitors.rb
+++
@@ -1,8 +0,0 @@
-class CreateVisitors < ActiveRecord::Migration
-  def change
-    create_table :visitors do |t|
-      t.string :ip
-      t.timestamps null: false
-    end
-  end
-end
--- test/controllers/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionController::TestCase
-  test "should get resume" do
-    get :resume
-    assert_response :success
-  end
-
-end
